1d41f50ef133f4c92ea1413ede561e13cb13737f
Octávio Delpupo <octavio.delpupo@gmail.com>
2022-01-30 21:57:58 -0300

    :heavy_check_mark:  python.

---
+++ EstruturaSequencial/4-exer/python.py
@@ -0,0 +1,11 @@
+nota1 = eval(input('Digite a primeira nota: '))
+
+nota2 = eval(input('Digite a segunda nota: '))
+
+nota3 = eval(input('Digite a terceira nota: '))
+
+nota4 = eval(input('Digite a quarta nota: '))
+
+soma = (nota1+nota2+nota3+nota4) /4
+
+print(f'A media do aluno e: {soma}')
