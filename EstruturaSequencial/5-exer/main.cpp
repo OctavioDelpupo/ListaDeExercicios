b42543d5db0b5ae3ed1a3a843744c70411510f17
Octávio Delpupo <octavio.delpupo@gmail.com>
2022-01-30 22:55:16 -0300

    :heavy_check_mark: c++

---
+++ EstruturaSequencial/5-exer/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    float cm, metro;
+
+    cout << " Digita a quantidade de metros para converter: ";
+    cin >> metro;
+
+    cm = metro * 100;
+
+    cout << "Quantidade de cm: " << cm >> endl;
+}
