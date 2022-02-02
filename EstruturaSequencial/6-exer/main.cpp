50f2a64b7c392c4d130088b9dd52bf51865b5d8b
Octávio Delpupo <octavio.delpupo@gmail.com>
2022-02-02 19:26:16 -0300

    :heavy_check_mark: C++

---
+++ EstruturaSequencial/6-exer/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    float raio,area;
+
+    cout << "Digite o raio deo clirculo: ";
+    cin >> raio;
+
+    area = raio * 3.14;
+
+    cout << "Area do circulo é: " << area << endl;
+}
