b860c50e00568a6d7f3a813763765945de615548
Octávio Delpupo <octavio.delpupo@gmail.com>
2022-02-10 21:35:50 -0300

    :heavy_check_mark: Python.

--- EstruturaSequencial/11-exer/README.md
+++ EstruturaSequencial/11-exer/README.md
@@ -15,7 +15,7 @@ c. o terceiro elevado ao cubo.
 - [✔️] _Fluxograma_
 - [✔️] _C++_
 - [✔️] _Pseudocódigo_
-- [ ] _Python_
+- [✔️] _Python_
 - [ ] _JavaScript_
 
 
---
+++ EstruturaSequencial/11-exer/pyton.py
@@ -0,0 +1,8 @@
+
+num1 = int(input("Digite o primeiro número intenrio: "))
+num2 = int(input("Digite o segundo número intenrio: "))
+num3 = float(input("Digite o terceiro número real: "))
+
+print("O produto do dobro do primeiro com metade do segundo: ", ((num1 * 2) * (num2 / 2)))
+print("A soma do triplo do primeiro com o terceiro: ", ((num1 * 3) + num3))
+print("O terceiro elevado ao cubo: ", num3 ** 3 )
